70949d377b499807ffe46541cd13d3cfa8276ed9
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-17 10:08:41 +0700

    menambhakan tag br agar tidak satu baris

--- Php/variable_konstanta.php
+++ Php/variable_konstanta.php
@@ -2,7 +2,7 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
-echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.<br>";
 
 $benar = true;
 $salah = false;
@@ -10,8 +10,8 @@ echo "Variable benar: $benar, Variable salah: $salah";
 
 // Mendefinikan konstanta untuk nilai tetap
 define("NAMA_SITUS" , "WebsiteKu.com");
-define("TAHUN_PENDIRIAN", 2023);
+define("TAHUN_PENDIRIAN", 2023 );
 
-echo "Selamat datang di " . NAMA_SITUS . ", situs yang 
+echo "<br>Selamat datang di " . NAMA_SITUS . ", situs yang 
 didirikan pada tahun ". TAHUN_PENDIRIAN . ".";
 ?>
