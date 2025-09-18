4ad2c8d6944e5e6b50655e31066a37c14a5cb909
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-18 13:32:31 +0700

    add tipe_data php

---
+++ Php/tipe_data.php
@@ -0,0 +1,15 @@
+<?php
+$a = 10;
+$b = 5;
+$c = $a + 5 ;
+$d = $b +(10 * 5);
+$e = $d - $c;
+
+echo "Variable a: {$a} <br>";
+echo "Variable b: {$b} <br>";
+echo "Variable b: {$c} <br>";
+echo "Variable b: {$d} <br>";
+echo "Variable b: {$e} <br>";
+
+var_dump($e);
+?>
