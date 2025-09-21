f9e6b776a7eb6e1b129621cd9ed9a8c0cc37ea73
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-21 19:05:52 +0700

    add Logika in Php

--- Php/Operator.php
+++ Php/Operator.php
@@ -15,3 +15,17 @@ echo "Hasil a * b = {$hasilKali} <br>";
 echo "Hasil a / b = {$hasilBagi} <br>";
 echo "Hasil a % b = {$sisaBagi} <br>";
 echo "Hasil a ** b = {$pangkat} <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil a == b ?: {$hasilSama} <br>";
+echo "Hasil a != b ?: {$hasilTidakSama}<br>";
+echo "Hasil a < b ?: {$hasilLebihKecil}<br>";
+echo "Hasil a > b ?: {$hasilLebihBesar}<br>";
+echo "Hasil a <= b ?: {$hasilLebihKecilSama}<br>";
+echo "Hasil a >= b ?: {$hasilLebihBesarSama}<br>";
