799141127711f8001f15cfb54b05c76af8540d4d
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-21 20:56:01 +0700

    add perulangan fori

--- Php/StrukturKontrol.php
+++ Php/StrukturKontrol.php
@@ -24,4 +24,17 @@ while ($jarakSaatIni < $jarakTarget) {
 }
 
 echo "<br> Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+// Perulangan For
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
 ?>
