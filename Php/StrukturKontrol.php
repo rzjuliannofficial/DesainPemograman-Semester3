85ea0973e0c7b42e2397c473146a519569a20874
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-21 20:55:26 +0700

    add kondisi at StrukturKontrol

--- Php/StrukturKontrol.php
+++ Php/StrukturKontrol.php
@@ -12,29 +12,6 @@ if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
     echo "Nilai huruf: D";
 }
 
-// Perulangan While
-$jarakSaatIni = 0;
-$jarakTarget = 500;
-$peningkatanHarian = 30;
-$hari = 0;
 
-while ($jarakSaatIni < $jarakTarget) {
-    $jarakSaatIni += $peningkatanHarian;
-    $hari++;
-}
-
-echo "<br> Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
-
-// Perulangan For
-$jumlahLahan = 10;
-$tanamanPerLahan = 5;
-$buahPerTanaman = 10;
-$jumlahBuah = 0;
-
-for ($i = 1; $i <= $jumlahLahan; $i++) {
-    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
-}
-
-echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 
 ?>
