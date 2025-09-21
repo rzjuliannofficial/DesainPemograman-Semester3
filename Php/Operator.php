d6275f337b5f0654e45276075537441c38333aad
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-21 14:24:19 +0700

    add Operator

---
+++ Php/Operator.php
@@ -0,0 +1,17 @@
+<?php 
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil a + b = {$hasilTambah} <br>";
+echo "Hasil a - b = {$hasilKurang} <br>";
+echo "Hasil a * b = {$hasilKali} <br>";
+echo "Hasil a / b = {$hasilBagi} <br>";
+echo "Hasil a % b = {$sisaBagi} <br>";
+echo "Hasil a ** b = {$pangkat} <br>";
