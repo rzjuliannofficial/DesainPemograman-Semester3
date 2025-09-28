967d0364a89ad2ed8a690331623d8c884033020e
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-28 22:17:18 +0700

    update multi_menu - periksa apa ada sub-menu -panggil fungsi secara rekursif

--- Php2/multi_menu.php
+++ Php2/multi_menu.php
@@ -19,8 +19,16 @@ $menu = [
 
 function tampilkanMenuBertingkat (array $menu) {
     echo "<ul>";
-    foreach ($menu as $m => $item) {
-        echo "<li>{$item['nama']}</li>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item['nama']}"; // Tampilkan item menu
+        
+        // Periksa apakah ada sub-menu
+        if (isset($item['subMenu']) && is_array($item['subMenu'])) {
+            // Panggil fungsi itu sendiri secara rekursif untuk sub-menu
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+        
+        echo "</li>";
     }
     echo "</ul>";
 }
