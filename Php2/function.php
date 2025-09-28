d9da74e62947e945626f8d0c49e952f3f0b64e10
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-28 21:31:04 +0700

    add function

---
+++ Php2/function.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan () {
+    echo "Assalamualaikum, <br/>";
+    echo "Perkenalkan, nama saya Elok<br/>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+?>
