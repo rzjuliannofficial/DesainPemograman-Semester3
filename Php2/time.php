a3bcc54bc99dca17d06876318b52405dc87908d8
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-29 11:09:17 +0700

    add time

---
+++ Php2/time.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Time</h3>
+    <?php
+        date_default_timezone_set("asia/jakarta");
+        echo date("h:i:sa");
+    ?>
+</body>
+</html>
