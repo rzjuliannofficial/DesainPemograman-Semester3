6eaeb3045741490c05b5556d330c1bab06d7c5db
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-29 11:08:18 +0700

    add date

---
+++ Php2/date.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Today is " . date("l") . "<br>"; // 1
+    echo "Today is " . date("Y/m/d") . "<br>"; // 2
+    echo "Today is " . date("Y.m.d") . "<br>"; // 3
+    echo "Today is " . date("Y-m-d") . "<br>"; // 4
+?>
