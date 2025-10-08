3cbab69f0fa8d9516c4ab1462518650d31d3cee4
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-08 09:37:17 +0700

    Pencocokan Digit dengan Kuantifier +

--- Form/regex.php
+++ Form/regex.php
@@ -10,3 +10,13 @@ if (preg_match($pattern, $text)) {
 }
 echo "<br><br>";
 
+echo "--- Soal 5.2 (Pencocokan Digit dengan Kuantifier +) ---<br>";
+$pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
+$text = 'There are 123 apples.';
+
+if (preg_match($pattern, $text, $matches)) { 
+    echo "Cocokkan: " . $matches[0]; // Output: Cocokkan: 123
+} else {
+    echo "Tidak ada yang cocok!";
+}
+echo "<br><br>";
