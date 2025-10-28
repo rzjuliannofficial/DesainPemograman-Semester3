c0c3d0b759470d129c566724df04afd144da3b4f
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-28 17:29:26 +0700

    update upload

--- Php_Form_Upload/Praktikum1/Upload.php
+++ Php_Form_Upload/Praktikum1/Upload.php
@@ -2,11 +2,20 @@
 if (isset($_POST['submit'])) {
     $target_dir = "uploads/"; //direktori yang dituju
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "File " . basename($_FILES["fileToUpload"]["name"]) . " berhasil diunggah.";
-    } else {
-        echo "Gagal mengunggah file.";
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5*1024*1024;
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "File " . basename($_FILES["fileToUpload"]["name"]) . " berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
+    } else{
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
+
