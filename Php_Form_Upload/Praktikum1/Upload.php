22acc3fcfc8588f2855fdb245de147d47469004c
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-28 17:26:29 +0700

    update

--- Php_Form_Upload/Praktikum1/Upload.php
+++ Php_Form_Upload/Praktikum1/Upload.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['submit'])) {
-    $target_dir = "uploads/";
+    $target_dir = "uploads/"; //direktori yang dituju
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
