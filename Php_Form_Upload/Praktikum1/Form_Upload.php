7da4a279b1335faf80e10974168893ef9307950c
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-28 18:01:15 +0700

    edit name atribut input

--- Php_Form_Upload/Praktikum1/Form_Upload.php
+++ Php_Form_Upload/Praktikum1/Form_Upload.php
@@ -4,7 +4,7 @@
 <body>
 <h2>Upload File</h2>
 <form action="upload.php" method="POST" enctype="multipart/form-data">
-  <input type="file" name="fileToUpload"><br><br>
+  <input type="file" name="myfile"><br><br>
   <input type="submit" name="submit" value="Upload File">
 </form>
 </body>
