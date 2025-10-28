7127241cf65480e9018b6f857aae9d0d81ecc1fc
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-28 18:01:25 +0700

    update

--- Php_Form_Upload/Praktikum1/Upload.php
+++ Php_Form_Upload/Praktikum1/Upload.php
@@ -1,21 +1,22 @@
 <?php
-if (isset($_POST['submit'])) {
-    $target_dir = "uploads/"; //direktori yang dituju
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+if(isset($_POST["submit"])){
+    $targetdir = "uploads/"; // Direktori tujuan untuk menyimpan file
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
     $allowedExtensions = array("jpg", "jpeg", "png", "gif");
     $maxsize = 5*1024*1024;
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "File " . basename($_FILES["fileToUpload"]["name"]) . " berhasil diunggah.";
-        } else {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah.";
+        }
+        else {
             echo "Gagal mengunggah file.";
         }
-    } else{
+    }
+    else{
         echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
-
